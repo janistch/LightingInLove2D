4d080b58800b0c8821e36abade1f6bebaf57bd65
janistch <40235449+janistch@users.noreply.github.com>
2021-01-10 19:47:20 +0100

    added crate and modified shader

--- main.lua
+++ main.lua
@@ -1,6 +1,8 @@
 local light = love.graphics.newShader([[
 
     extern vec2 origin;
+    extern float radius;
+    extern float fallof;
 
     vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords)
     {
@@ -8,8 +10,14 @@ local light = love.graphics.newShader([[
 
         float dist = distance(origin, screen_coords);
 
-        if (dist > 50) {
-            currentColor.a = (300 - dist)/250;
+        float safeFallof = fallof;
+
+        if (fallof > radius) {
+            safeFallof = radius;
+        }
+
+        if (dist > safeFallof) {
+            currentColor.a = (radius - dist)/(radius - safeFallof);
         }
 
         return vec4(1, 0.87, 0.52, currentColor.a);
@@ -21,16 +29,16 @@ bgSource = love.graphics.newImage("assets/tiles.png")
 bgSource:setWrap("repeat", "repeat")
 bgQuad = love.graphics.newQuad(0, 0, 800, 600, bgSource:getWidth(), bgSource:getHeight())
 
-
-local fakeLight = {
-    x = 500,
-    y = 100
-}
+box = love.graphics.newImage("assets/box.png")
 
 -- The "player" which is also our light source
 local player = {
     x = 400,
     y = 300
+}
+
+local newEdge = {
+    x1 = 0, y1 = 0, x2 = 0, y2 = 0, draw = false
 }
 
 -- All the edges of our world
@@ -39,24 +47,22 @@ local edges = {
     { x1 = 10, y1 = 590, x2 = 790, y2 = 590 },
     { x1 = 10, y1 = 10, x2 = 10, y2 = 590 },
     { x1 = 790, y1 = 10, x2 = 790, y2 = 590 },
-    { x1 = 150, y1 = 200, x2 = 200, y2 = 100 },
-    { x1 = 200, y1 = 100, x2 = 250, y2 = 150 },
-    { x1 = 500, y1 = 350, x2 = 600, y2 = 250 },
-    { x1 = 300, y1 = 500, x2 = 450, y2 = 500 },
-    { x1 = 250, y1 = 420, x2 = 400, y2 = 420 },
+
+    -- { x1 = 150, y1 = 200, x2 = 200, y2 = 100 },
+    -- { x1 = 200, y1 = 100, x2 = 250, y2 = 150 },
+    -- { x1 = 500, y1 = 350, x2 = 600, y2 = 250 },
+    -- { x1 = 300, y1 = 500, x2 = 460, y2 = 510 },
+    -- { x1 = 250, y1 = 420, x2 = 400, y2 = 420 },
+
+    { x1 = 500, y1 = 100, x2 = 564, y2 = 100 },
+    { x1 = 500, y1 = 164, x2 = 564, y2 = 164 },
+    { x1 = 500, y1 = 100, x2 = 500, y2 = 164 },
+    { x1 = 564, y1 = 100, x2 = 564, y2 = 164 },
 }
 
 -- The polygon which represents the visible area
 local visiblePolygon = {}
 local aPolygon = {}
-
--- Checks if two line segments intersect
--- Returns coordinates if they do and false if they don't
-function segmentIntersect(x1, y1, x2, y2)
-
-
-
-end
 
 -- The main attraction: this function calculates the visible area in form of a polygon
 function calculateVisiblePolygon(originX, originY, radius)
@@ -194,8 +200,9 @@ end
 
 function love.load()
 
-    love.graphics.setLineStyle("rough")
+    love.window.setTitle("Lighting")
     love.window.setVSync(0)
+    love.graphics.setLineStyle("rough")
 
     calculateVisiblePolygon(player.x, player.y, 2000)
 
@@ -204,10 +211,33 @@ end
 function love.update(dt)
 
     if love.mouse.isDown(1) then
+
         player.x = love.mouse.getX()
         player.y = love.mouse.getY()
 
         calculateVisiblePolygon(player.x, player.y, 2000)
+
+    end
+
+end
+
+function love.mousepressed(mx, my, btn)
+
+    if btn == 2 and not newEdge.draw then
+        newEdge.draw = true
+        newEdge.x1 = mx
+        newEdge.y1 = my
+    end
+
+end
+
+function love.mousereleased(mx, my, btn)
+
+    if btn == 2 and newEdge.draw then
+        newEdge.draw = false
+        newEdge.x2 = mx
+        newEdge.y2 = my
+        table.insert(edges, { x1=newEdge.x1, y1=newEdge.y1, x2=newEdge.x2, y2=newEdge.y2 })
     end
 
 end
@@ -230,6 +260,8 @@ function love.draw()
         -- draw Polygon
         love.graphics.setShader(light)
             light:send("origin", { player.x, player.y })
+            light:send("radius", 300)
+            light:send("fallof", 20)
             for _, triangle in ipairs(aPolygon) do
                 love.graphics.polygon("fill", triangle)
             end
@@ -237,15 +269,21 @@ function love.draw()
     
     love.graphics.setCanvas()
 
+    -- Draw light
+    love.graphics.setBlendMode("multiply", "premultiplied")
+    love.graphics.draw(canvas)
+    love.graphics.setBlendMode("alpha", "alphamultiply")
+
     -- Draw the player
     love.graphics.setColor(0,0.1,0.5)
     love.graphics.circle("fill", player.x, player.y, 10, 5)
     love.graphics.setColor(1,1,1)
 
-    -- Draw light
-    love.graphics.setBlendMode("multiply", "premultiplied")
-    love.graphics.draw(canvas)
-    love.graphics.setBlendMode("alpha", "alphamultiply")
+    love.graphics.draw(box, 500, 100)
+
+    -- for k, pt in ipairs(visiblePolygon) do
+    --     love.graphics.print(k, pt[2], pt[3])
+    -- end
 
     -- Draw all edges
     for k, edge in ipairs(edges) do
@@ -253,5 +291,7 @@ function love.draw()
     end
 
     love.graphics.print("FPS: "..love.timer.getFPS(), 10, 10)
+    love.graphics.printf("Hold the left mouse button to move the player around.", 0, 550, 800, "center")
+    love.graphics.printf("Hold the right mouse button to create a wall.", 0, 570, 800, "center")
 
 end
